4666018e3ee363d1d58e795a8020650b40e52058
Quang Duy Ta <taquangduy1228@gmail.com>
2020-08-23 20:08:24 +0700

    'buoi2_update'

--- reactfe49/src/RenderWithMap/RenderWithMap.js
+++ reactfe49/src/RenderWithMap/RenderWithMap.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
 
 export default class RenderWithMap extends Component {
-    productList=[
-        {id=1, name:'iphone X', price:1000},
-        {id=2, name:'Samsung Note 10plus', price:3000},
-        {id=3, name:'Htc m10', price:2000}
+    productList = [
+        {id:1,name:'Iphone X',price:1000},
+        {id:2,name:'Samsung Note 10plus',price:3000},
+        {id:3,name:'Htc m10',price:2000},
     ]
 
     renderProduct= ()=> {
     // let content=[];
-    // for (let i=0; i<= this.producList.length; i++){
-    //         let product= this.producList[i];
+    // for (let i=0; i<= this.productList.length; i++){
+    //         let product= this.productList[i];
     //         let cardProduct= <div key={i} className="col-4">
     //   <div className="card text-left">
     //     <img className="card-img-top" src="https://picsum/photos/200/200" alt />
@@ -24,13 +24,13 @@ export default class RenderWithMap extends Component {
     //     }
     //     return content;
 
-    let arrJSXProduct= this.producList.map((product, index)=> {
+    let arrJSXProduct= this.productList.map((product, index)=> {
     return <div key={index} className="col-4">
     <div className="card text-left">
-    <img className="card-img-top" src="https://picsum/photos/200/200" alt />
+    <img className="card-img-top" src="https://picsum.photos/200/200" alt />
     <div className="card-body">
-    <h4 className="card-title">{'{'}product.name{'}'}</h4>
-    <p className="card-text">{'{'}product.price{'}'}</p>
+    <h4 className="card-title">{product.name}</h4>
+    <p className="card-text">{product.price}</p>
     </div>
     </div>
     </div>;
